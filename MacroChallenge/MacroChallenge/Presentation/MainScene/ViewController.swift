b9798deff45b0c37a1c539c6aafff526ee9e5959
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-10-12 18:01:57 +0700

    [INITIAL] Project

---
+++ MacroChallenge/MacroChallenge/Presentation/MainScene/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MacroChallenge
+//
+//  Created by Arif Luthfiansyah on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
