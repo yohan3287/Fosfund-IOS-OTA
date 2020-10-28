b554d5d489ceb9fc94314c8105117fa4a63ffbf3
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-10-28 16:10:57 +0700

    Add SignInUI

---
+++ MacroChallenge/MacroChallenge/Presentation/SignInUI/SignInView.swift
@@ -0,0 +1,26 @@
+//
+//  SignInView.swift
+//  MacroChallenge
+//
+//  Created by Arif Luthfiansyah on 28/10/20.
+//
+
+import UIKit
+
+class SignInView: UIView {
+
+    var controller: SignInController!
+
+    func viewDidLoad() {
+        
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
