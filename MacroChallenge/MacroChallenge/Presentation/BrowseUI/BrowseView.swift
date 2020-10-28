93d7bfed640f89ba4f2b091a832c08fb998a16ca
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-10-28 16:06:39 +0700

    Add BrowseUI & JenjangAnakAsuhUI

---
+++ MacroChallenge/MacroChallenge/Presentation/BrowseUI/BrowseView.swift
@@ -0,0 +1,20 @@
+//
+//  BrowseView.swift
+//  MacroChallenge
+//
+//  Created by Arif Luthfiansyah on 28/10/20.
+//
+
+import UIKit
+
+class BrowseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
