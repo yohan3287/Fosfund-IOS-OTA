a71039f45fab219c00f6277bf48e101c0bc7980b
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-10-28 16:02:54 +0700

    Add OnBoardingUI

---
+++ MacroChallenge/MacroChallenge/Presentation/OnBoardingUI/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  MacroChallenge
+//
+//  Created by Arif Luthfiansyah on 28/10/20.
+//
+
+import UIKit
+
+class OnBoardingView: UIView {
+
+    var controller: OnBoardingController!
+
+    func viewDidLoad() {
+    }
+
+
+}
