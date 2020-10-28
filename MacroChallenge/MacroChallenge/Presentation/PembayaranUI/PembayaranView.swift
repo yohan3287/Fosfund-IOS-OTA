3c4373f375aa6ae5560826ed3f9817be10a2ef02
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-10-28 16:13:26 +0700

    Add PembayaranUI

---
+++ MacroChallenge/MacroChallenge/Presentation/PembayaranUI/PembayaranView.swift
@@ -0,0 +1,26 @@
+//
+//  PembayaranView.swift
+//  MacroChallenge
+//
+//  Created by Arif Luthfiansyah on 28/10/20.
+//
+
+import UIKit
+
+class PembayaranView: UIView {
+
+    var controller: PembayaranController!
+
+    func viewDidLoad() {
+        
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
